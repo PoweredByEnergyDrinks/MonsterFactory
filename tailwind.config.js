a30538af52a2cee30afaa4eee50744708f015d03
Rudi Boshoff <Rudi@agrigate.co.za>
2021-12-11 22:32:16 +0200

    Add feature images

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,24 @@ module.exports = {
       },
       backgroundImage: {
         'hero': "url('/assets/branding/hero.png')",
+        'feature-1': "url('/assets/branding/feature-1.png')",
+        'feature-2': "url('/assets/branding/feature-2.png')",
+        'feature-3': "url('/assets/branding/feature-3.png')",
+      },
+      animation: {
+        'point': 'point 2s infinite',
+      },
+      keyframes: {
+        point: {
+          '0%, 100%': {
+            transform: 'translateX(-10%)', 
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)'
+          },
+          '50%': {
+            'transform': 'translateX(0)',
+            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)'
+          }
+        }
       }
     }
   },
