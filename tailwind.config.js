bfc1f4a3f139473a726273688e58fd056e5e4662
Rudi Boshoff <Rudi@agrigate.co.za>
2021-12-12 01:45:05 +0200

    services section and styling improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'feature-1': "url('/assets/branding/feature-1.png')",
         'feature-2': "url('/assets/branding/feature-2.png')",
         'feature-3': "url('/assets/branding/feature-3.png')",
+        'service-1': "url('/assets/branding/service-1.png')",
+        'service-2': "url('/assets/branding/service-2.png')",
+        'service-3': "url('/assets/branding/service-3.png')",
       },
       animation: {
         'point': 'point 2s infinite',
